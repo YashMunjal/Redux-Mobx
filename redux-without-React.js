0bde25b9758e91d0e0c215be6b24a2451e849bea
yashmunjal <yashmu45@gmail.com>
2018-08-24 20:19:19 +0530

    start

--- redux-without-React.js
+++ redux-without-React.js
@@ -1,22 +1,96 @@
 const {
-  createStore,
-  combineReducers,
-  compose,
-  bindActionCreators,
-  applyMiddleware
-} = Redux;
-
-const makeLouder=string.toUpperCase();
-const embolden = string.bold();
-
-const makeLouderAndEmbolden=compose(embolden,makeLouder);
-console.log(makeLouderAndEmbolden('hello'));
-// Output: <b>HELLO</b>
-
-const reducer=(state={value:1},action)=>{
+    createStore,
+    combineReducers,
+    compose,
+    bindActionCreators,
+    applyMiddleware
+  } = Redux;
+  
+  
+  const initialState = { result: 0 };
+  
+  const addAction = {
+    type: 'ADD',
+    value: 4
+  };
+  
+  const calculatorReducer = (
+    state = initialState,
+    action
+  ) => {
+    if (action.type === 'ADD') {
+      return {
+        ...state,
+        result: state.result + action.value
+      }
+    }
+    return state;  
+  }
+  
+  const subscriber = () => {
+    console.log('SUBSCRIPTION!!!!', store.getState())
+  };
+  
+  const logger = ({ getState }) => {
+    return next => action => {
+      console.log(
+        'MIDDLEWARE',
+        getState(),
+        action
+      );
+      const value = next(action);
+      console.log({value});
+      return value;
+    }
+  }
+  
+  const initialError = { message: '' };
+  
+  let errorMessageReducer = (
+    state = initialError,
+    action
+  ) => {
+    if (action.type === 'SET_ERROR_MESSAGE')
+      return { message: action.message };
+    if (action.type === 'CLEAR_ERROR_MESSAGE')
+      return { message: '' };
     return state;
-}
-
-const store=createStore(reducer);
-
-store.dispatch({type:'ADD'},payload:{});
+  };
+  
+  const store = createStore(combineReducers({
+    calculator: calculatorReducer,
+    error: errorMessageReducer
+  }), {}, applyMiddleware(logger));
+  
+  const unsubscribe = store.subscribe(subscriber);
+  
+  const add = value => ({ type: 'ADD', value });
+  
+  const setError = (message) => (
+    { type: 'SET_ERROR_MESSAGE', message}
+  );
+  
+  const clearError = () => (
+    { type: 'CLEAR_ERROR_MESSAGE' }
+  );
+  
+  const bindActionCreator =
+        (action, dispatch) => 
+          (...args) => dispatch(action(...args));
+  
+  const addValue = bindActionCreator(add, store.dispatch);
+  
+  const bindActionCreatorz = (actions, dispatch) => {
+    return Object.keys(actions).reduce((boundActions, key) => {
+      boundActions[key] = bindActionCreator(actions[key], dispatch);
+      return boundActions;
+    }, {});
+  }
+  
+  const errors = bindActionCreatorz({ 
+    set: setError,
+    clear: clearError
+  }, store.dispatch);
+  
+  
+  
