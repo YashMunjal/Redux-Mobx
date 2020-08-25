7b5a7218d63a5aeafe3eb0e1b7f3653c981f98f3
yashmunjal <yashmu45@gmail.com>
2020-08-26 00:02:40 +0530

    start

---
+++ redux-without-React.js
@@ -0,0 +1,20 @@
+const {
+  createStore,
+  combineReducers,
+  compose,
+  bindActionCreators,
+  applyMiddleware
+} = Redux;
+
+const makeLouder=string.toUpperCase();
+const embolden = string.bold();
+
+const makeLouderAndEmbolden=compose(embolden,makeLouder);
+console.log(makeLouderAndEmbolden('hello'));
+// Output: <b>HELLO</b>
+
+const reducer=(state={value:1},action)=>{
+    return state;
+}
+
+const store=createStore(reducer);
